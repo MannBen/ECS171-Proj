2422e43fe520efc3dc9cce66abfe1d1686bcb816
Ben Mann <bnmann@ucdavis.edu>
2023-04-30 19:28:54 -0700

    updated pngscript and new pokemon2 dir with both png and jpeg as png

--- Run.py
+++ Run.py
@@ -45,6 +45,12 @@ class Run:
                                 transforms.ToTensor(),
                                 transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
                             ]))
+        # dataset = dset.ImageFolder(dir,
+        #             transform=transforms.Compose([
+        #                 transforms.Resize(64),
+        #                 transforms.CenterCrop(64),
+        #                 transforms.ToTensor(),
+        #             ]))
         #dataset = dset.ImageFolder(dir)
 
         dataObj = torch.utils.data.DataLoader(dataset, batch_size=128, shuffle=True)
--- pngscript.py
+++ pngscript.py
@@ -1,10 +1,15 @@
 import os
 from PIL import Image
 for image in os.listdir('../ECS171-Proj/images/pokemon/'):
-  print(image)
-  im = Image.open('../ECS171-Proj/images/pokemon/' + image)
-  # If is png image
-  if im.format == 'PNG':
-    # and is not RGBA
-    if im.mode != 'RGBA':
-      im.convert("RGBA").save(f"../ECS171-Proj/images/pokemon2/{image}2.png")
+    print(image)
+    im = Image.open('../ECS171-Proj/images/pokemon/' + image)
+    # If is png image
+    if im.format == 'PNG':
+        # and is not RGBA
+        if im.mode != 'RGBA':
+            im.convert("RGBA").save(f"../ECS171-Proj/images/pokemon2/{image}2.png")
+        else:
+            im.save(f"../ECS171-Proj/images/pokemon2/{image}2.png")
+    elif im.format == 'JPEG':
+        print("enter")
+        im.convert("RGBA").save(f"../ECS171-Proj/images/pokemon2/{image}2.png")

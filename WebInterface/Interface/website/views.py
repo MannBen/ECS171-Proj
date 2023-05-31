209471fd4756b3faa17182b1447cff090788440d
Amy Law <121009837+amylaw2@users.noreply.github.com>
2023-05-30 20:16:27 -0700

    Delete WebInterface directory

--- WebInterface/Interface/website/views.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, render_template
-
-views = Blueprint('views', __name__)
-
-@views.route('/')
-
-def home():
-    condition = True
-    return render_template("home.html")

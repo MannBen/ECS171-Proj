c7449ae7bad05a40a423a2b0bb8d1e0564835a32
Ben Mann <bnmann@ucdavis.edu>
2023-06-06 20:24:52 -0700

    finalized code/images/

--- pngscript.py
+++ pngscript.py
@@ -1,15 +1,15 @@
 import os
 from PIL import Image
-for image in os.listdir('../ECS171-Proj/images/pokemon/'):
+for image in os.listdir('./images/pokemon2/'):
     print(image)
-    im = Image.open('../ECS171-Proj/images/pokemon/' + image)
+    im = Image.open('./images/pokemon2/' + image)
     # If is png image
     if im.format == 'PNG':
         # and is not RGBA
         if im.mode != 'RGBA':
-            im.convert("RGBA").save(f"../ECS171-Proj/images/pokemon2/{image}2.png")
+            im.convert("RGBA").save(f"./images/pokemon3/{image}")
         else:
-            im.save(f"../ECS171-Proj/images/pokemon2/{image}2.png")
+            im.save(f"./images/pokemon3/{image}")
     elif im.format == 'JPEG':
         print("enter")
-        im.convert("RGBA").save(f"../ECS171-Proj/images/pokemon2/{image}2.png")
+        im.convert("RGBA").save(f"./images/pokemon3/{image}")
